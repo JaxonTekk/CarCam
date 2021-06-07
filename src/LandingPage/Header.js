ed65abfea82ae397cdb94e8ea30eeb2717cdb668
23yimichael <23yimichael@gmail.com>
2021-06-07 15:19:43 -0700

    Header Styling

--- src/LandingPage/Header.js
+++ src/LandingPage/Header.js
@@ -2,18 +2,38 @@ import React from 'react'
 import {StyleSheet, View, Text} from 'react-native'
 
 export default function Header() {
-    let d = new Date().getHours()%12 + ":" + (new Date().getMinutes()/10===0?"0"+new Date().getMinutes():new Date().getMinutes()) + " " + (new Date().getHours()<12?"AM":"PM");
+    let t = new Date().getHours()%12 + ":" + (new Date().getMinutes()/10<1?"0"+new Date().getMinutes():new Date().getMinutes()) + " " + (new Date().getHours()<12?"AM":"PM");
 
     return (
-        <View styles={styles.container}>
-            <View styles={styles.border}>
-                <Text>Welcome!</Text>
-                <Text>{d}</Text>
-            </View>
-            
+        <View style={styles.container}>
+            <Text style={styles.welcome}>Welcome</Text>
+            <Text style={styles.time}>{t}</Text> 
         </View>
     );
 }
 
 const styles = StyleSheet.create({
+    container: {
+        height: 100, 
+        backgroundColor: '#fff',
+        padding: 2, 
+        borderBottomLeftRadius: 30,
+        borderBottomRightRadius: 30,
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.5,
+        shadowRadius: 3.84,
+    },
+
+    welcome: {
+        marginTop: 5,
+        fontSize: 25
+    },
+
+    time: {
+        fontSize: 30,
+        color: '#7c587f'
+    }
 });
--- src/LandingPage/LandingPageViewController.js
+++ src/LandingPage/LandingPageViewController.js
@@ -3,11 +3,13 @@ import React from 'react';
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import Header from './Header'
 import Footer from '../Footer'
+import Statistics from './Components/Statistics';
 
 export default function LandingPageViewController() {
   return (
       <SafeAreaView>
           <Header/>
+          <Statistics/>
           <Footer/>
       </SafeAreaView>
   );

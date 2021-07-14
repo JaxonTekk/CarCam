da6fd8162f19419942fa045a38c9c927a5603343
23yimichael <23yimichael@gmail.com>
2021-07-13 22:57:06 -0700

    added bottom tab icons

--- src/LandingPage/LandingPageViewController.js
+++ src/LandingPage/LandingPageViewController.js
@@ -2,6 +2,9 @@ import React from "react";
 import { Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
+import MaterialIcons from "react-native-vector-icons/MaterialIcons";
+import Ocitcons from "react-native-vector-icons/Octicons";
 import Home from "./Screens/Home.js";
 import Record from "./Screens/Record.js";
 import Settings from "./Screens/Settings.js";
@@ -19,9 +22,36 @@ export default function App() {
           inactiveTintColor: "white",
         }}
       >
-        <Tab.Screen name="Home" component={Home} />
-        <Tab.Screen name="Record" component={Record} />
-        <Tab.Screen name="Settings" component={Settings} />
+        <Tab.Screen
+          name="Home"
+          component={Home}
+          options={{
+            tabBarLabel: "Home",
+            tabBarIcon: ({ color }) => (
+              <FontAwesome5 name="home" color={color} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Record"
+          component={Record}
+          options={{
+            tabBarLabel: "Home",
+            tabBarIcon: ({ color }) => (
+              <MaterialIcons name="fiber-smart-record" color={color} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Settings"
+          component={Settings}
+          options={{
+            tabBarLabel: "Home",
+            tabBarIcon: ({ color }) => (
+              <Ocitcons name="file-directory" color={color} />
+            ),
+          }}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );

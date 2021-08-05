2baf1b0bd1d07e71d2b6db74ba400e9748b27e74
Jason Chen <californiaroad1@gmail.com>
2021-08-05 15:22:06 -0700

    fixed mph issue

--- src/screens/ViewRecordings.js
+++ src/screens/ViewRecordings.js
@@ -1,10 +1,39 @@
 import React from "react";
-import { SafeAreaView, Text } from "react-native";
+import { SafeAreaView, Text, View, StyleSheet, Dimensions, TouchableOpacity, Image } from "react-native";
 
 export default function ViewRecordings() {
+  const RecordingCard = (props) => {
+    return (
+      <View styles={styles.recordingsContainer}>
+          
+      </View>
+    );
+  }
+
   return (
     <SafeAreaView>
-      <Text>View Recordings</Text>
+      <View style={styles.recordingsListContainer}>
+        <Text style={styles.yearHeadingText}>2021</Text>
+        <TouchableOpacity>
+          <RecordingCard/>
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  recordingsListContainer: {
+    marginLeft: Dimensions.get("window").height / 55,
+    marginTop: 20,
+    marginRight: Dimensions.get("window").height / 55,
+    flexDirection: "column",
+  },
+  yearHeadingText: {
+    fontSize: 23,
+    fontFamily: "Nunito-Bold"
+  },
+  recordingsContainer: {
+    flexDirection: "row",
+  }
+});

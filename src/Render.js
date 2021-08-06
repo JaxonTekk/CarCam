c11bbecb8822c1576f7f036931d188b5a4110284
23yimichael <23yimichael@gmail.com>
2021-08-05 19:29:14 -0700

    Fixed fonts not loading

--- src/Render.js
+++ src/Render.js
@@ -1,20 +1,22 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { ActivityIndicator } from "react-native";
 import { useFonts } from "expo-font";
 import * as Font from "expo-font";
 import TabNavigator from "./components/TabNavigator.js";
 
 export default function Render() {
-  const [loaded] = useFonts({
-    NunitoRegular: require("./assets/fonts/Nunito/Nunito-Regular.ttf"),
-    NunitoBold: require("./assets/fonts/Nunito/Nunito-Bold.ttf"),
-  });
+  const [loaded, setLoaded] = useState(false);
 
-  useEffect(async () => {
+  const loadFonts = async () => {
     await Font.loadAsync({
-      "Nunito-Regular": require("./assets/fonts/Nunito/Nunito-Regular.ttf"),
-      "Nunito-Bold": require("./assets/fonts/Nunito/Nunito-Bold.ttf"),
+      "Nunito-Regular": require("../assets/fonts/Nunito/Nunito-Regular.ttf"),
+      "Nunito-Bold": require("../assets/fonts/Nunito/Nunito-Bold.ttf"),
     });
+    setLoaded(true);
+  };
+
+  useEffect(() => {
+    loadFonts();
   }, []);
 
   if (!loaded) {

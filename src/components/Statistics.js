3390aed88aa46b9613460a86cf97a22a7b6748f3
23yimichael <23yimichael@gmail.com>
2021-08-11 18:33:09 -0600

    Fixed memoryValue bugs

--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -1,58 +1,60 @@
 import React, { useContext, useState, useEffect } from "react";
 import { View, Text, StyleSheet, Dimensions } from "react-native";
-import { AnimatedCircularProgress } from 'react-native-circular-progress';
+import { AnimatedCircularProgress } from "react-native-circular-progress";
 import * as FileSystem from "expo-file-system";
 import Context from "../utils/Context.js";
 import { memo } from "react/cjs/react.production.min";
 
 export default function Statistics() {
   const { videoCount } = useContext(Context);
-  const [memoryValue, setMemory] = useState(75);
+  const [memoryValue, setMemoryValue] = useState(75);
   const [videos, setVideos] = useState(0);
 
   const [usedStorage, setUsedStorage] = useState(0);
   const [availableStorage, setAvailableStorage] = useState(0);
 
   const read = async () => {
-    await FileSystem.getFreeDiskStorageAsync().then((freeDiskStorage) => {
-      setUsedStorage(freeDiskStorage);
-    });
-    await FileSystem.getTotalDiskCapacityAsync().then((totalDiskCopacity) => {
-      setAvailableStorage(totalDiskCopacity);
-    });
-
-    await setMemory(((availableStorage - usedStorage) / availableStorage) * 100);
-    await console.log(memoryValue)
+    await FileSystem.getFreeDiskStorageAsync().then((freeDiskStorage) =>
+      setUsedStorage(freeDiskStorage)
+    );
+    await FileSystem.getTotalDiskCapacityAsync().then((totalDiskCopacity) =>
+      setAvailableStorage(totalDiskCopacity)
+    );
+    setMemoryValue(
+      Math.floor(((availableStorage - usedStorage) / availableStorage) * 100)
+    );
+    console.log(memoryValue);
   };
 
   useEffect(() => {
     read();
   }, []);
 
-  setTimeout( () => {
-    setMemory(memoryValue)
+  setTimeout(() => {
+    setMemory(memoryValue);
   }, 10000);
 
+  if (!memoryValue) return <Text>Loading</Text>;
+
   return (
-  <View style={{alignItems: "center", marginTop: 30}}>
-    <AnimatedCircularProgress
-      size={Dimensions.get("window").width / 1.6}
-      width={13}
-      fill={memoryValue}
-      backgroundWidth={6}
-      rotation={(0, -360)}
-      tintColor="#007F97"
-      lineCap="round"
-      backgroundColor="#D7EAEE">
-      {
-        (fill) => (
+    <View style={{ alignItems: "center", marginTop: 30 }}>
+      <AnimatedCircularProgress
+        size={Dimensions.get("window").width / 1.6}
+        width={13}
+        fill={memoryValue}
+        backgroundWidth={6}
+        rotation={(0, -360)}
+        tintColor="#007F97"
+        lineCap="round"
+        backgroundColor="#D7EAEE"
+      >
+        {(fill) => (
           <View>
             <Text style={styles.largeText}>{videoCount}</Text>
             <Text style={styles.smallText}>{memoryValue}</Text>
           </View>
-        )
-      }
-     </AnimatedCircularProgress>
+        )}
+      </AnimatedCircularProgress>
     </View>
   );
 }
@@ -62,12 +64,12 @@ const styles = StyleSheet.create({
     fontSize: 100,
     color: "#051126",
     textAlign: "center",
-    fontFamily: "Nunito-Regular"
+    fontFamily: "Nunito-Regular",
   },
   smallText: {
     fontSize: 20,
     color: "#A4BCBC",
     textAlign: "center",
-    fontFamily: "Nunito-Light"
+    fontFamily: "Nunito-Light",
   },
 });
